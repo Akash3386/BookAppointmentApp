431f26528aefba0fd7bf03d915e6a0f48790d0eb
Akash3386 <pandey.akash33862gmail.com>
2022-11-15 10:08:35 +0530

    objects in local storage

--- Appointment.js
+++ Appointment.js
@@ -4,35 +4,38 @@ const emailInput = document.querySelector('#email');
 const msg = document.querySelector('.msg');
 const userList = document.querySelector('#users');
 
-// Listen for form submit
+// // Listen for form submit
 myForm.addEventListener('submit', onSubmit);
 
 function onSubmit(e) {
   e.preventDefault();
   
   if(nameInput.value === '' || emailInput.value === '') {
-    // alert('Please enter all fields');
+    alert('Please enter all fields');
     msg.classList.add('error');
     msg.innerHTML = 'Please enter all fields';
 
-    // Remove error after 3 seconds
+//     // Remove error after 3 seconds
     setTimeout(() => msg.remove(), 3000);
   } else {
-    // Create new list item with user
-    // const li = document.createElement('li');
+// //     // Create new list item with user
+// //     // const li = document.createElement('li');
 
-    // Add text node with input values
-    // li.appendChild(document.createTextNode(`${nameInput.value}: ${emailInput.value}`));
+// //     // Add text node with input values
+// //     // li.appendChild(document.createTextNode(`${nameInput.value}: ${emailInput.value}`))
+    let myObj = {
+      name : nameInput.value,
+      email : emailInput.value
+    }
+    localStorage.setItem("userDetails",JSON.stringify(myObj)); //get change
+// //     // localStorage.setItem(nameInput.value,emailInput.value);
+// //     // Add HTML
+// //     // li.innerHTML = `<strong>${nameInput.value}</strong>e: ${emailInput.value}`;
 
-    localStorage.setItem(`${nameInput.value}`,`${emailInput.value}`); //get change
+// //     // Append to ul
+// //     // userList.appendChild(li);
 
-    // Add HTML
-    // li.innerHTML = `<strong>${nameInput.value}</strong>e: ${emailInput.value}`;
-
-    // Append to ul
-    // userList.appendChild(li);
-
-    // Clear fields
+// //     // Clear fields
     nameInput.value = '';
     emailInput.value = '';
     
