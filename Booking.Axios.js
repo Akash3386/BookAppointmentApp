217565b7e76caa956a8468fb9db595224a06e5dc
Akash3386 <pandey.akash3386@gmail.com>
2022-12-30 11:38:00 +0530

    update user

--- Booking.Axios.js
+++ Booking.Axios.js
@@ -7,7 +7,7 @@ const userList = document.querySelector('#users');
 // Listen for form submit
 myForm.addEventListener('submit', onSubmit);
 userList.addEventListener("click",removeItems);
-// userList.addEventListener("click",editItem);
+userList.addEventListener("click",editItem);
 
 // To get detail on screen
 axios.get('https://crudcrud.com/api/5f5c4ab67c074290a36d45a323d6e7c0/BookingData')
@@ -34,6 +34,7 @@ function onSubmit(e) {
      axios.post('https://crudcrud.com/api/5f5c4ab67c074290a36d45a323d6e7c0/BookingData',myObj)
           .then((response) =>{
             showPost(response.data);
+            
             console.log(response);
           })
           .catch((error)=>{
@@ -57,6 +58,47 @@ function removeItems(e){
 
 }
 
+function editItem(e){
+  e.preventDefault();
+  if (e.target.classList.contains('edit')){
+        
+    var li = e.target.parentElement;
+
+    axios.get(`https://crudcrud.com/api/5f5c4ab67c074290a36d45a323d6e7c0/BookingData/${li.id}`)
+     .then((response) => {
+        // showUserOnScreen(response.data);
+        nameInput.value = response.data.name;
+        emailInput.value = response.data.email;
+     })
+
+    userList.removeChild(li);
+
+    if(nameInput.value === '' || emailInput.value === '') {
+      // alert('Please enter all fields');
+      msg.classList.add('error');
+      msg.innerHTML = 'Please enter all fields';
+  
+      // Remove error after 3 seconds
+      setTimeout(() => msg.remove(), 3000);
+    }
+    else{
+      axios.put(`https://crudcrud.com/api/5f5c4ab67c074290a36d45a323d6e7c0/BookingData/${li.id}`,{
+      name: nameInput.value,
+      email: emailInput.value
+    })
+    .then((response)=>{
+      //  showPost(response.data);
+      console.log(response.data);
+    })
+    .catch(err=> console.log(err))
+
+    }
+
+    
+  }
+
+}
+
 
 function showUserOnScreen(object){
     
@@ -72,6 +114,7 @@ function showUserOnScreen(object){
       var editBtn = document.createElement('button');
       
       deleteBtn.className = " float-right delete";
+      editBtn.className = "edit";
 
       deleteBtn.appendChild(document.createTextNode('DELETE'));
       editBtn.appendChild(document.createTextNode('EDIT'));
@@ -95,6 +138,7 @@ function showPost(object){
     var editBtn = document.createElement('button');
       
     deleteBtn.className = " float-right delete";
+    editBtn.className = "edit";
 
     
     deleteBtn.appendChild(document.createTextNode('DELETE'));
